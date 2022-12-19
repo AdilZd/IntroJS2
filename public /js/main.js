2e9dc84cbc5dee4e3444f0d83e92ae6982cc46fe
AdilZd <nowayat0me@gmail.com>
2022-12-19 11:48:38 +0100

    Test

---
+++ public /js/main.js
@@ -0,0 +1,50 @@
+// Exo 1 //
+
+let bouffe = "J’aime les tartines et j’en mange";
+console.log (bouffe);
+
+// Exo 2 //
+
+let CmbJours = " 4 par jours";
+console.log (CmbJours);
+
+
+// Exo 3 //
+
+bouffe + CmbJours 
+console.log (bouffe + CmbJours)
+
+
+// Exo 4 //
+
+let trois = 5;
+
+
+// Exo 5 //
+
+let quatre = 35;
+trois + quatre;
+console.log (trois + quatre);
+
+
+// Exo 6 //
+
+bouffe + " " + trois
+console.log(bouffe + " " + trois)
+
+
+// Exo 7 //
+
+let salut = "Salut tout le monde";
+console.log(salut);
+
+let aurevoir= "Aurevoir !";
+console.log(aurevoir);
+
+salut = aurevoir 
+
+// Exo 8 //
+
+let Four = " 4 ";
+let Five = 5;
+Four > Five;
